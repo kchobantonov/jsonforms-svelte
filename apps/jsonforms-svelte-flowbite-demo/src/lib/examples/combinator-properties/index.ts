869fbc0a434fc0ca0e1b2fdadf237e665446ea0a
Krasimir Chobantonov <kchobantonov@gmail.com>
2026-02-21 21:36:33 -0500

    fix the typeing issues

--- apps/jsonforms-svelte-flowbite-demo/src/lib/examples/combinator-properties/index.ts
+++ apps/jsonforms-svelte-flowbite-demo/src/lib/examples/combinator-properties/index.ts
@@ -1,3 +1,4 @@
+import type { JsonSchema } from '@jsonforms/core';
 import data from './data.json' with { type: 'json' };
 import schema from './schema.json' with { type: 'json' };
 import uischema from './uischema.json' with { type: 'json' };
@@ -7,7 +8,7 @@ export default [
     name: 'combinator-properties',
     label: 'Combinator Properties',
     data: data,
-    schema: schema,
-    uischema: uischema
+    schema: schema as unknown as JsonSchema,
+    uischema: uischema,
   },
 ];
